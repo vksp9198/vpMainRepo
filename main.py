3832a955f4abd7c97b2bdcbd93ed6a26de91dbdf
vikasMain_git91 <vp9198776636@gmail.com>
2026-09-24 11:09:09 +0530

    remove extra space

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def read_line(file_name , number_of_lines):
     with open(file_name) as f:
         lines = (f.readlines()[-number_of_lines:])
         for line in lines:
-            print(line)
+            print(line, end="")
+            
 
 file_name = "user_data.txt"
 number_of_lines = 3
