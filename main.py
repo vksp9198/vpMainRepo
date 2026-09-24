252a0976e52324475b5ec91e600057bffa6c25b0
vikasMain_git91 <vp9198776636@gmail.com>
2026-09-24 10:44:41 +0530

    read n lines of a file

---
+++ main.py
@@ -0,0 +1,15 @@
+# read last n lines ot a file
+def read_line(file_name , number_of_lines):
+    with open(file_name) as f:
+        lines = (f.readlines()[-number_of_lines:])
+        for line in lines:
+            print(line)
+
+file_name = "user_data.txt"
+number_of_lines = 3
+
+
+try:
+    read_line(file_name , number_of_lines)
+except:
+    print("file not found")
